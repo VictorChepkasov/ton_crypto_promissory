61e3cc0452fa4f301bb0a905195c0526ba9adc1b
Victor Chepkasov <victorsugary@gmail.com>
2024-01-08 23:25:28 +0500

    add msgValue and little comments

--- tests/CryptoPromissory.spec.ts
+++ tests/CryptoPromissory.spec.ts
@@ -1,14 +1,10 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano, fromNano } from 'ton-core';
+import { toNano } from 'ton-core';
 import { PromissoryMaster } from '../wrappers/PromissoryMaster';
 import '@ton/test-utils';
 import { Promissory } from '../wrappers/Promissory';
-import { stringify } from 'querystring';
-import { captureRejectionSymbol } from 'events';
 
 describe('Crypto Promissory Tests', () => {
-    const minTonForStorage = toNano("0.3")
-
     let blockchain: Blockchain;
     let promissoryMaster: SandboxContract<PromissoryMaster>;
     let promissory: SandboxContract<Promissory>
@@ -20,6 +16,7 @@ describe('Crypto Promissory Tests', () => {
     let promissoryAmount = 4321n
     let promissoryFee = 2n
     let dateOfClose = 1707013424n
+    let msgValue = toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee)
 
     const replacer = (key: any, value: any) =>
         typeof value === "bigint" ? value.toString() : value;
@@ -32,6 +29,7 @@ describe('Crypto Promissory Tests', () => {
         drawer = await blockchain.treasury('drawer')
         newHolder = await blockchain.treasury('newHolder')
 
+        // deploy
         await promissoryMaster.send(
             deployer.getSender(),
             {
@@ -43,6 +41,7 @@ describe('Crypto Promissory Tests', () => {
             }
         );
 
+        // create promissory
         await promissoryMaster.send(drawer.getSender(), {
             value: toNano("0.3")
         }, {
@@ -53,6 +52,7 @@ describe('Crypto Promissory Tests', () => {
             dateOfClose: dateOfClose
         })
 
+        // create promissory instance
         promissory = blockchain.openContract(Promissory.fromAddress(await promissoryMaster.getPromissoryAddressByIndex(0n)))
     });
 
@@ -78,6 +78,7 @@ describe('Crypto Promissory Tests', () => {
     });
 
     it('promissory transfer', async () => {
+        // transfer from holder.address to newHolder.address
         await promissory.send(holder.getSender(), {
             value: toNano("0.3")
         }, {
@@ -92,63 +93,69 @@ describe('Crypto Promissory Tests', () => {
             drawer: drawer.address,
             holder: newHolder.address,
             id: 0n,
-            promissoryAmount: toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee),
+            promissoryAmount: msgValue,
             promissoryFee: promissoryFee,
             dateOfClose: dateOfClose,
             closed: false
         }
-
         expect(JSON.stringify(promissoryInfo, replacer)).toEqual(JSON.stringify(validPromissoryInfo, replacer))
     })
 
     it('pay promissory', async () => {
         console.log(`Master: ${promissoryMaster.address}\nProissory: ${promissory.address}\nDrawer: ${drawer.address}\nHolder: ${holder.address}\nNew Holder: ${newHolder.address}`)
-        blockchain.now = 1707013425
+        // create evironment
         let beforeMasterBalancee: bigint = await promissoryMaster.getMasterBalance()
+        blockchain.now = 1707013425
 
+        // action
         await promissory.send(drawer.getSender(), {
-            value: toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee)
+            value: msgValue
         }, "pay")
         
         let promissoryInfo = await promissory.getPromissoryInfo()
-        
         let validPromissoryInfo = {
             '$$type': 'PromissoryInfo',
             drawer: drawer.address,
             holder: holder.address,
             id: 0n,
-            promissoryAmount: toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee),
+            promissoryAmount: msgValue,
             promissoryFee: promissoryFee,
             dateOfClose: dateOfClose,
             closed: true
         }
-
         expect(JSON.stringify(promissoryInfo, replacer)).toEqual(JSON.stringify(validPromissoryInfo, replacer))
         expect(await promissoryMaster.getMasterBalance()).toBeGreaterThan(beforeMasterBalancee)
     })
 
     it('withdraw promissory', async () => {
+        // create environment
         let promissoryBeforeBalance: bigint = await promissory.getPromissoryBalance()
         blockchain.now = 1707013425
         await promissory.send(drawer.getSender(), {
-            value: toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee)
+            value: msgValue
         }, "pay")
 
+        // action
         await promissory.send(holder.getSender(), {
             value: toNano("0.03")
         }, "withdraw")
+
         expect(await promissory.getPromissoryBalance()).toBeLessThan(promissoryBeforeBalance)
     })
 
     it('withdraw promissory fee', async () => {
+        // create environment
         blockchain.now = 1707013425
         await promissory.send(drawer.getSender(), {
-            value: toNano(promissoryAmount) + (toNano(promissoryAmount) / 100n * promissoryFee)
+            value: msgValue
         }, "pay")
         let beforeMasterBalancee: bigint = await promissoryMaster.getMasterBalance()
+
+        // action
         await promissoryMaster.send(deployer.getSender(), {
             value: toNano("0.03")
         }, "withdraw")
+
         expect(await promissoryMaster.getMasterBalance()).toBeLessThan(beforeMasterBalancee)
     })
 });
